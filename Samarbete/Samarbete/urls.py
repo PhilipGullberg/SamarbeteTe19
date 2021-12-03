8f5af5c3052fdab25da701e282acc98031aefd74
Carlos <carlos.karat@elev.ga.ntig.se>
2021-12-03 08:51:19 +0100

    commit on Carlos

--- Samarbete/Samarbete/urls.py
+++ Samarbete/Samarbete/urls.py
@@ -5,7 +5,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Person 1, Använd include för att kopp tilla mainsite.urls, register.urls och shop.urls
-    path('',)),
-    path('register/', ),
-    path('shop/', ),
+    path('mainsite/', include('mainsite.urls')),
+    path('register/', include('register.urls') ),
+    path('shop/', include('shop.urls') ),
 ]
